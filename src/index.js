2ad01acba366f0a70fbdd84dc5f8a6f11fd2e8ce
Gravewalker666 <heshanandrews99@gmail.com>
2021-04-22 19:07:05 +0530

    Add bill gates quote section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
     $(".nav-item").click(function () {
         $(".nav-item").removeClass("text-5xl font-bold");
-        $(".nav-item").addClass("text-sm");
         $(this).addClass("text-5xl font-bold");
-        $(this).removeClass("text-sm");
         let sectionId = $(this).attr('id').split("-")[1];
         changeScene(sectionId);
     });
